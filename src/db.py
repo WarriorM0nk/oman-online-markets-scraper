b8704be57c8667ac18b29a8787f940bf2f438d9f
WarriorM0nk <149814337+WarriorM0nk@users.noreply.github.com>
2025-08-08 23:07:45 +0400

    Added functions in db.py

--- src/db.py
+++ src/db.py
@@ -5,3 +5,12 @@ def connect(db_host, db_name, db_username, db_password):
     conn = psycopg2.connect(conn_string)
 
     return conn
+
+def fetch_config(connection, child_table_name, parent_name, parent_table_name):
+    with connection:
+        with connection.cursor() as curs:
+            curs.execute(
+                f'SELECT * FROM {child_table_name}' \
+                f'   WHERE {parent_name} = %s;',
+            (parent_table_name[0], ))
+            return curs.fetchall()
--- src/master.py
+++ src/master.py
@@ -1,8 +1,7 @@
 import dotenv
 import os
-import json
-
 import db
+import docker
 
 
 # GLOBAL SETTINGS
@@ -16,8 +15,44 @@ DB_USER     = os.getenv('POSTGRES_USER')
 DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
 
 
-# Initiates a connection to the database
+# Instantiates a connection to the database
 conn = db.connect(DB_HOST, DB_NAME, DB_USER, DB_PASSWORD)
+
+# Get list of websites to be scraped
+with conn:
+    with conn.cursor() as curs:
+        curs.execute(
+            'SELECT * FROM website_config'
+        )
+        website_list = curs.fetchall()
+
+# Scraper containers will be instantiated 
+# based on the number of websites fetched
+# using the docker python API
+
+# RETURN VAL:
+# [
+#     (id, name, url),
+#     (id, name, url),
+#     ...
+# ]
+
+# FIXME: nesting should not start from the
+# website for loop as the other containers will
+# be waiting
+for website in website_list:
+    category_list = db.fetch_config(conn, 'category_config', 'website', website)
+
+for category in category_list:
+    subcategory_list = db.fetch_config(conn, 'subcategory_config', 'category', category)
+
+    for subcategory in subcategory_list:
+        element_list = db.fetch_config(conn, 'element_config', 'subcategory', subcategory)
+
+        for element in element_list:
+            pass
+                
+
 
 # Closes the connection to the database
 conn.close()
